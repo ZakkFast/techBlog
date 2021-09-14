fbd87c4625497093d826ee88590494817de92cba
Zakk Fast <zakkfastpro@gmail.com>
2021-09-14 16:41:59 -0700

    basic server running

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
