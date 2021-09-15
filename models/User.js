13edd45004328f7c5a0a994f2af8922d57f17d72
Zakk Fast <zakkfastpro@gmail.com>
2021-09-14 17:10:54 -0700

    added password auth and model export

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
+const { update } = require("lodash");
+const { FAILSAFE_SCHEMA } = require("js-yaml");
 
 class User extends model {
   checkPassword(logingPassword) {
@@ -36,4 +38,23 @@ User.init(
       },
     },
   },
+  {
+      hooks: {
+          beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10)
+              return newUserData
+          },
+          beforeUpdate: async (updatedUserData) => {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
+              return updatedUserData
+          }
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'User',
+  }
 );
+
+module.exports = User;
