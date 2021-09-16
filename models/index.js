bc7cb4930d72f9e89c4cfad0c55f67ec8f98c6cb
Zakk Fast <zakkfastpro@gmail.com>
2021-09-15 21:49:12 -0700

    added foreignKeys

--- models/index.js
+++ models/index.js
@@ -1,5 +1,25 @@
-const User = require('./User')
-const Posts = require('./Posts')
-const Comments = require('./Comments')
+const User = require("./User");
+const Posts = require("./Posts");
+const Comments = require("./Comments");
 
-module.exports = { User, Posts, Comments}
+Posts.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Posts, {
+  foreignKey: "user_id",
+});
+
+Posts.hasMany(Comments, {
+  foreignKey: "post_id",
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: "post_id",
+});
+
+Comments.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Posts, Comments };
