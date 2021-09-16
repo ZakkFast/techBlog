6f4509f2b073617d14973dda657a2d0fe834e4b0
Zakk Fast <zakkfastpro@gmail.com>
2021-09-15 18:01:48 -0700

    completed post model

--- models/Posts.js
+++ models/Posts.js
@@ -1,5 +1,37 @@
-/*
-post id
-post body
-foreign key for user
-*/
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+postMessage.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    subject_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTablesName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
